35e0a590cfc0553db51215dc0bff53f8c2000ae2
Jordan Hudgens <jordan@devcamp.com>
2017-07-26 05:07:39 +0000

    Added Stuff

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,9 @@
 class Portfolio < ApplicationRecord
+  validates_presence_of :title
+  
+  def self.blah
+    where(subtitle: "Blah dee Blah") 
+  end
+  
+  scope :ruby_on_rails, -> {where(subtitle: "Ruby on Rails")}
 end
